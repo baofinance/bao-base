2055b8f11e079d7ae0b98d37040df528c609b0fc
rootminus0x1 <rootminus0x1@gmail.com>
2024-10-03 21:44:56 +0100

    latest

--- src/interfaces/IOwnable.sol
+++ src/interfaces/IOwnable.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.26;
 import { IERC5313 } from "@openzeppelin/contracts/interfaces/IERC5313.sol";
 
 /// @notice Simple single owner authorization mixin.
-/// @author Solady (https://github.com/vectorized/solady/blob/main/src/auth/Ownable.sol)
+/// @author interface from Solady (https://github.com/vectorized/solady/blob/main/src/auth/Ownable.sol)
 ///
 /// @dev Note:
 /// This implementation does NOT auto-initialize the owner to `msg.sender`.
