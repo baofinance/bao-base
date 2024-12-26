37c8a63a7887abdd652f8cfbb647e3ebd15d6502
rootminus0x1 <rootminus0x1@gmail.com>
2024-12-26 20:26:23 +0000

    upgrade solc to 0.8.28

--- src/internal/BaoCheckOwner.sol
+++ src/internal/BaoCheckOwner.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.28;
 
 /// @title Bao Check Owner
 /// @dev Note:
--- test/BaoOwnableTransferrable.t.sol
+++ test/BaoOwnableTransferrable.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.28;
 
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import {IERC5313} from "@openzeppelin/contracts/interfaces/IERC5313.sol";
