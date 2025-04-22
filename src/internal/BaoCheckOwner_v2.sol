b906d230930bc66e8b1cf8db38996d693a4a8da5
rootminus0x1 <rootminus0x1@gmail.com>
2025-04-22 14:12:36 +0100

    fix slither error

--- src/internal/BaoCheckOwner_v2.sol
+++ src/internal/BaoCheckOwner_v2.sol
@@ -43,6 +43,8 @@ abstract contract BaoCheckOwner_v2 {
 
     // @inheritdoc IBaoOwnable
     function _owner() internal view virtual returns (address owner_) {
+        // check against the transfer time to see if it has happened
+        // slither-disable-next-line timestamp
         owner_ = (block.timestamp >= _OWNER_TRANSFER_AT) ? _OWNER_AT : _BEFORE_OWNER;
     }
 
