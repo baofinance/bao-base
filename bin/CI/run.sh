9e0692ab207fa2001eb0019154d241822f62e3e4
rootminus0x1 <rootminus0x1@gmail.com>
2025-06-10 08:56:05 +0100

    handle submodules in yarn CI

--- bin/CI/run.sh
+++ bin/CI/run.sh
@@ -9,13 +9,15 @@ cleanup() {
   local exit_code=$?
   # Check if temp_dir exists and is not empty
   if [[ -n "${temp_dir:-}" && -d "${temp_dir}" ]]; then
-    echo "Cleaning up temporary directory: ${temp_dir}"
+    log "Cleaning up temporary directory: ${temp_dir}"
     rm -rf "${temp_dir}"
   fi
+  mutex_release "act"
+  mutex_release "act-submodule"
 
   # Output error message if script failed
   if [[ ${exit_code} -ne 0 ]]; then
-    echo "Error: Script exited with status ${exit_code}" >&2
+    error "CI exited with status ${exit_code}" >&2
   fi
 
   exit "${exit_code}"
@@ -53,48 +55,63 @@ while [[ "$#" -gt 0 ]]; do
   esac
 done
 
-echo "Running CI for ${foundry_version} foundry on ${os_version}"
+log "Running CI for ${foundry_version} foundry on ${os_version}"
 
+# create a temporary directory to hold modified events and workflow files (this is not the temp directory act uses)
 # shellcheck disable=SC2154
 temp_dir="${BAO_BASE_TOOLS_DIR}/act-cache/$$"
 mkdir -p "${temp_dir}"
-echo "Created temporary directory: ${temp_dir}"
+log "Created temporary directory: ${temp_dir}"
 
 workflow_template_file="${dep_dir}/local_test_${workflow}.yml"
 workflow_file="${temp_dir}/local_test_${workflow}_${os_version}_${foundry_version}.yml"
 event_template_file="${dep_dir}/workflow_dispatch.json"
 event_file="${temp_dir}/workflow_dispatch_${os_version}_${foundry_version}.json"
 
-echo "replacing \$OS_VERSION with '${os_version}' and \$FOUNDY_VERSION with '${foundry_version}' in ${event_template_file}"
+# handle submodules - in submodules .git is a file, in the root, it is a directory
+if [[ -f .git ]]; then
+  mutex_acquire "act-submodule"
+  rm ~/.cache/act/.git
+  # get the root directory, basically 2 levels up
+  parent_repo_root=$(git rev-parse --show-superproject-working-tree)
+  # we know where the act cache directory is - and two levels up just happens to be somewhere we can access
+  ln -s ${parent_repo_root}/.git ~/.cache/act/.git
+fi
+
+# hack the event file to hardcode the os_version and foundry_version to the values passed in (or defaulted)
+log "replacing \$OS_VERSION with '${os_version}' and \$FOUNDY_VERSION with '${foundry_version}' in ${event_template_file} > ${event_file}"
 # shellcheck disable=SC2154 # we don't need to check if the variable is set
 sed "s|\$OS_VERSION|${os_version}|g" "${event_template_file}" | sed "s|\$FOUNDRY_VERSION|${foundry_version}|g" >"${event_file}"
 
+# hack the workflow file to hardcode the correct directory
 # shellcheck disable=SC2154
-echo "replacing \$BAO_BASE_DIR with './${BAO_BASE_DIR}' in ${workflow_file}"
+log "replacing \$BAO_BASE_DIR with './${BAO_BASE_DIR}' in ${workflow_template_file} > ${workflow_file}"
 # shellcheck disable=SC2154 # we don't need to check if the variable is set
 sed "s|\$BAO_BASE_DIR|./${BAO_BASE_DIR}|g" "${workflow_template_file}" >"${workflow_file}"
 
+# stop multiple instances of this script installing act at the same time
 mutex_acquire "act"
 
 # shellcheck disable=SC2154
 if [[ ! -x "${BAO_BASE_TOOLS_DIR}/act/act" ]]; then
-  info 0 "installing act..."
+  log "installing act..."
   mkdir -p "${BAO_BASE_TOOLS_DIR}/act"
   # shellcheck disable=SC2154
   if [[ "${BAO_BASE_OS}" == "linux" ]]; then
     curl https://raw.githubusercontent.com/nektos/act/master/install.sh | bash -s -- -b "${BAO_BASE_TOOLS_DIR}/act"
     if [[ ! -x "${BAO_BASE_TOOLS_DIR}/act/act" ]]; then
-      echo "act installation failed"
-      exit 1
+      error "act installation failed"
     fi
   elif [[ "${BAO_BASE_OS}" == "macos" ]]; then
     brew install act
   else
-    echo "operating system not supported yet"
+    error "operating system not supported yet"
   fi
 fi
 
+# let them go
 mutex_release "act"
 
-echo act -P ubuntu-latest=-self-hosted -W "${workflow_file}" -e "${event_file}" "$@"
-"${BAO_BASE_TOOLS_DIR}"/act/act -P ubuntu-latest=-self-hosted -W "${workflow_file}" -e "${event_file}" "$@"
+"${BAO_BASE_TOOLS_DIR}"/act/act -P ubuntu-latest=-self-hosted -W "${workflow_file}" -e "${event_file}" "$@" || error "CI run failed"
+log "CI run succeeded"
+mutex_release "act-submodule"
