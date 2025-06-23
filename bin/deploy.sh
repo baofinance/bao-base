a3f74b157adbb6c6308f0c1ebae1b641835bfb53
rootminus0x1 <rootminus0x1@gmail.com>
2025-06-23 15:20:39 +0100

    fixed some anvil/chain_id interactions

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -24,10 +24,13 @@ debug "deploy.sh: ${args[*]}"
 # set the global variables needed for transacting/recording etc
 # default to environment
 PRIVATE_KEY=$(lookup_environment PRIVATE_KEY)
+sensitive "${PRIVATE_KEY}" "private-key"
+PUBLIC_KEY=$(public_from_private "${PRIVATE_KEY}")
 ETHERSCAN_API_KEY=$(lookup_environment ETHERSCAN_API_KEY)
+sensitive "${ETHERSCAN_API_KEY}" "etherscan-api-key"
 
 # only allow this to be command line as it's dangerous
-RPC_URL="anvil"
+RPC_URL="local"
 VERIFY="--verify"
 BROADCAST=""
 LOCAL="remote"
@@ -39,49 +42,45 @@ debug "myargs: ${myargs[*]}"
 args=()
 while [[ ${#myargs[@]} -gt 0 ]]; do
   case "${myargs[0]}" in
-  --rpc-url)
-    RPC_URL="${myargs[1]}"
-    myargs=("${myargs[@]:2}") # shift 2
-    ;;
-  --no-verify)
-    VERIFY=""
-    myargs=("${myargs[@]:1}") # shift 1
-    ;;
-  --broadcast)
-    debug "setting BROADCAST"
-    # shellcheck disable=SC2034
-    BROADCAST="--broadcast"
-    myargs=("${myargs[@]:1}") # shift 1
-    ;;
-  -h | --help)
-    echo "Usage: ${0} [--rpc-url <url>] [--no-verify] [--broadcast] <script> [<args>...]"
-    echo "  --rpc-url <url>   Specify the RPC URL to use (default: anvil)"
-    echo "                    \"anvil\" is the same as -rpc-url local, but using a test private key"
-    echo "  --no-verify       Skip verification of contracts on Etherscan, default is to verify on non-local RPC URLs"
-    echo "  --broadcast       Broadcast transactions (default is no broadcasting, i.e. dry-run)"
-    echo "  <script>          The script to run after setting up the environment"
-    echo "  <args>...        Additional arguments to pass to the script"
-    exit 0
-    ;;
-  *)
-    args+=("${myargs[0]}")
-    myargs=("${myargs[@]:1}") # shift 1
-    ;;
+    --anvil)
+      log "--rpc-local anvil is the saame as -rpc-url local, but using a test private key"
+      PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+      PUBLIC_KEY="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+      RPC_URL="local"
+      CHAIN_ID=1                # this is used for address lookup
+      myargs=("${myargs[@]:1}") # shift 1
+      ;;
+    --rpc-url)
+      RPC_URL="${myargs[1]}"
+      myargs=("${myargs[@]:2}") # shift 2
+      ;;
+    --no-verify)
+      VERIFY=""
+      myargs=("${myargs[@]:1}") # shift 1
+      ;;
+    --broadcast)
+      debug "setting BROADCAST"
+      # shellcheck disable=SC2034
+      BROADCAST="--broadcast"
+      myargs=("${myargs[@]:1}") # shift 1
+      ;;
+    -h | --help)
+      echo "Usage: ${0} [--rpc-url <url>] [--no-verify] [--broadcast] <script> [<args>...]"
+      echo "  --rpc-url <url>   Specify the RPC URL to use (default: anvil)"
+      echo "                    \"anvil\" is the same as -rpc-url local, but using a test private key"
+      echo "  --no-verify       Skip verification of contracts on Etherscan, default is to verify on non-local RPC URLs"
+      echo "  --broadcast       Broadcast transactions (default is no broadcasting, i.e. dry-run)"
+      echo "  <script>          The script to run after setting up the environment"
+      echo "  <args>...        Additional arguments to pass to the script"
+      exit 0
+      ;;
+    *)
+      args+=("${myargs[0]}")
+      myargs=("${myargs[@]:1}") # shift 1
+      ;;
   esac
 done
 debug "args: ${args[*]}"
-
-# override the private key in certain circumstances
-if [[ "${RPC_URL}" == "anvil" ]]; then
-  log "--rpc-local anvil is the saame as -rpc-url local, but using a test private key"
-  PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-  PUBLIC_KEY="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-  # anvil is another name for local
-  RPC_URL="local"
-else
-  sensitive "${PRIVATE_KEY}" "private-key"
-  PUBLIC_KEY=$(public_from_private "${PRIVATE_KEY}")
-fi
 
 # determine if we're running locally or not
 if [[ "${RPC_URL}" == "local" || "${RPC_URL}" == *"localhost"* ]]; then
@@ -90,10 +89,8 @@ if [[ "${RPC_URL}" == "local" || "${RPC_URL}" == *"localhost"* ]]; then
   VERIFY="" # can't verify locally (yet)
 fi
 
-sensitive "${ETHERSCAN_API_KEY}" "etherscan-api-key"
-
 # get the chain_id
-CHAIN_ID=$(chain_id) || error "Failed to get chain ID from RPC URL: ${RPC_URL}"
+CHAIN_ID=${CHAIN_ID:-$(chain_id)} || error "Failed to get chain ID from RPC URL: ${RPC_URL}"
 
 log "transacting on${LOCAL:+ ${LOCAL}} chain ${CHAIN_ID}${CHAIN_NAME:+ (${CHAIN_NAME})}" # lint-bash disable=command-substitution
 log "using wallet with public key $(ens_from_public "${PUBLIC_KEY}")"                    # lint-bash disable=command-substitution
