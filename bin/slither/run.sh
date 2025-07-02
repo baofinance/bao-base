fbda6c0a23521f73304676f0d5a46d56661b19e0
rootminus0x1 <rootminus0x1@gmail.com>
2025-07-02 07:41:49 +0100

    fixed validate

--- bin/slither/run.sh
+++ bin/slither/run.sh
@@ -5,5 +5,5 @@ arg=${1:-.}
 
 # shellcheck disable=SC2154 # we don't need to check if the variable is set
 log "slither v$("$BAO_BASE_BIN_DIR"/run-python slither --version)"
-export FOUNDRY_PROFILE=slither
+export FOUNDRY_PROFILE=novyper
 "$BAO_BASE_BIN_DIR"/run-python slither "$arg" --config "$BAO_BASE_DIR/slither.config.json" --exclude-dependencies --fail-pedantic
