df8f7b81e35f489ddd2cebae1cb52a9c259ea2df
rootminus0x1 <rootminus0x1@gmail.com>
2025-10-31 08:29:29 +0000

    update regression and test lint issues

--- test/mocks/upgradeable/MockMinter.sol
+++ test/mocks/upgradeable/MockMinter.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.8.28 <0.9.0;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import {BaoOwnable} from "@bao/BaoOwnable.sol";
 import {BaoOwnableRoles} from "@bao/BaoOwnableRoles.sol";
 
 /**
