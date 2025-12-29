28720f40a46a431ab88f2b4719513c7350fc0445
rootminus0x1 <rootminus0x1@gmail.com>
2025-12-29 21:46:29 +0000

    make slither more flexible

--- bin/slither/run.sh
+++ bin/slither/run.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-arg=${1:-.}
-
 # shellcheck disable=SC2154 # we don't need to check if the variable is set
 log "slither v$("$BAO_BASE_BIN_DIR"/run-python slither --version)"
 export FOUNDRY_PROFILE=novyper
-"$BAO_BASE_BIN_DIR"/run-python slither "$arg" --config "$BAO_BASE_DIR/slither.config.json" --exclude-dependencies --fail-pedantic
+"$BAO_BASE_BIN_DIR"/run-python slither . --config "$BAO_BASE_DIR/slither.config.json" --exclude-dependencies --fail-pedantic "$@"
