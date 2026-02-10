f14760473903b63b45ec2b6082770e0d6caf7895
rootminus0x1 <rootminus0x1@gmail.com>
2026-02-10 21:55:16 +0000

    remove vyper support

--- bin/slither/run.sh
+++ bin/slither/run.sh
@@ -3,5 +3,4 @@ set -euo pipefail
 
 # shellcheck disable=SC2154 # we don't need to check if the variable is set
 log "slither v$("$BAO_BASE_BIN_DIR"/run-python slither --version)"
-export FOUNDRY_PROFILE=novyper
 "$BAO_BASE_BIN_DIR"/run-python slither . --config "$BAO_BASE_DIR/slither.config.json" --exclude-dependencies --fail-pedantic "$@"
