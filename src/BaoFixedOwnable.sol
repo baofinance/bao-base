f561ed1b82d1915f50e6478c5e52a7c6ee595576
rootminus0x1 <rootminus0x1@gmail.com>
2026-04-20 06:57:38 +0100

    Revert "add BaoFixedOwnable as backwards-compat alias for HarborFixedOwnable"
    This reverts commit ccae979e5019b6f32367910308065033276e68f3.

--- src/BaoFixedOwnable.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.28 <0.9.0;
-
-import {HarborFixedOwnable} from "@bao/HarborFixedOwnable.sol";
-
-/// @notice Backwards-compatibility alias for HarborFixedOwnable.
-abstract contract BaoFixedOwnable is HarborFixedOwnable {}
