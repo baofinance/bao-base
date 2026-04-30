4099955a4a8642a056c1e668d8905a79a4bb9fbd
rootminus0x1 <rootminus0x1@gmail.com>
2026-04-30 06:11:16 +0100

    update slither

--- bin/slither/run.sh
+++ bin/slither/run.sh
@@ -2,5 +2,7 @@
 set -euo pipefail
 
 # shellcheck disable=SC2154 # we don't need to check if the variable is set
+# Fix hash randomisation so slither's analysis is deterministic across platforms
+export PYTHONHASHSEED=0
 log "slither v$("$BAO_BASE_BIN_DIR"/run-python slither --version)"
 "$BAO_BASE_BIN_DIR"/run-python slither . --config "$BAO_BASE_DIR/slither.config.json" --exclude-dependencies --fail-pedantic "$@"
